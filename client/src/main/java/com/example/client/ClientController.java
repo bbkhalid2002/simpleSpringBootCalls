2ef9e28814127bbb340a6d4d1ca8f7ee37ff168a
bbkhalid2002 <75424759+bbkhalid2002@users.noreply.github.com>
2020-12-03 14:04:59 +0300

    First version

---
+++ client/src/main/java/com/example/client/ClientController.java
@@ -0,0 +1,26 @@
+package com.example.client;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ClientController {
+
+	@GetMapping
+	public String index() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.TEXT_PLAIN);
+		HttpEntity<String> reqeust = new HttpEntity<String>("Khalid", headers);
+		RestTemplate restTemplate = new RestTemplate();
+		
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity("https://localhost:8443/server", reqeust, String.class);
+		
+		
+		return "Server working and status("+String.valueOf(responseEntity.getStatusCode())+")";
+	}
+}
---
+++ server/src/main/java/com/example/server/HomeController.java
@@ -0,0 +1,35 @@
+package com.example.server;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping
+	public String index() {
+		return "Server working ...";
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> sayHello(@RequestBody medto name) {
+		return new ResponseEntity<String>("Hello " + name.getName(),HttpStatus.OK);
+	}
+}
+
+class medto {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
